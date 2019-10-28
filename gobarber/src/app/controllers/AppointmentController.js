918c42d31459d8f75d6d9e03d2f6b97fc3fbd351
Pedro Meneghel <pedromeneghel@gmail.com>
2019-10-28 08:21:15 -0300

    Feat: send cancellation email

--- gobarber/src/app/controllers/AppointmentController.js
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -5,18 +5,32 @@ import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
+import Mail from '../../lib/Mail';
 
 class AppointmentController {
   async destroy(req, res) {
-    const appointament = await Appointment.findByPk(req.params.id);
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
+      ],
+    });
 
-    if (appointament.user_id !== req.userId) {
+    if (appointment.user_id !== req.userId) {
       return res.status(401).json({
         error: "You don't have permission to cancel this appointment",
       });
     }
 
-    const dateWithSub = subHours(appointament.date, 2);
+    const dateWithSub = subHours(appointment.date, 2);
 
     if (isBefore(dateWithSub, new Date())) {
       return res
@@ -24,11 +38,24 @@ class AppointmentController {
         .json({ error: 'You can onle cancel appontments 2 hours in advance.' });
     }
 
-    appointament.canceled_at = new Date();
+    appointment.canceled_at = new Date();
 
-    await appointament.save(appointament);
+    await appointment.save(appointment);
 
-    return res.json(appointament);
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      template: 'cancellations',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "dd 'de' MMMM', às' H:mm'h'", {
+          locale: pt,
+        }),
+      },
+    });
+
+    return res.json(appointment);
   }
 
   async index(req, res) {
---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fddcf19cc85a0a',
+    pass: '94fea945599e3e',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreplay@gobarber.com',
+  },
+};
