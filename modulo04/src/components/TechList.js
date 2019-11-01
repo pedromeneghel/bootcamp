478d898788f3345f75955e2b49b6d0dd97078bd5
Pedro Meneghel <pedromeneghel@gmail.com>
2019-11-01 06:57:58 -0300

    Feat: remover item da lista

--- modulo04/src/components/TechList.js
+++ modulo04/src/components/TechList.js
@@ -11,18 +11,42 @@ class TechList extends Component {
   }
 
   handleInputChange = e => {
-    console.log(e.target.value)
+    this.setState({ newTech: e.target.value });
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ 
+      techs: [ ...this.state.techs, this.state.newTech ],
+      newTech: ''
+    })
+  }
+
+  handleDelete = (tech) => {
+    this.setState({
+      techs: this.state.techs.filter(t => t !== tech)
+    })
   }
 
   render() {
 
     return (
-      <>
+      <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button type="button" onClick={() => this.handleDelete(tech)}>Remover</button>
+            </li>
+          ))}
         </ul>
-        <input type="text" onChange={this.handleInputChange} />
-      </>
+        <input 
+          type="text" 
+          onChange={this.handleInputChange} 
+          value= {this.state.newTech} 
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
