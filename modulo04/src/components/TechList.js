2adb8e7d40888623b793f9e354591caf00fe5fac
Pedro Meneghel <pedromeneghel@gmail.com>
2019-11-01 06:44:10 -0300

    Finalizado configuração do ambiente react

---
+++ modulo04/src/components/TechList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: [
+      'Node.Js',
+      'ReactJS',
+      'React Native'
+    ]
+  }
+
+  handleInputChange = e => {
+    console.log(e.target.value)
+  }
+
+  render() {
+
+    return (
+      <>
+        <ul>
+          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+        </ul>
+        <input type="text" onChange={this.handleInputChange} />
+      </>
+    );
+  }
+}
+
+export default TechList;
