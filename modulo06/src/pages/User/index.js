286e2689b9612c98cd16daebfd370df4c1a38422
Pedro Meneghel <pedromeneghel@gmail.com>
2019-11-19 22:54:40 -0300

    modulo 06 finalizado

--- modulo06/src/pages/User/index.js
+++ modulo06/src/pages/User/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import propTypes from 'prop-types';
+
+import PropTypes from 'prop-types';
 
 import api from '../../services/api';
 
@@ -9,11 +10,12 @@ import {
   Avatar,
   Name,
   Bio,
+  Loading,
   Stars,
   Starred,
+  OwnerAvatar,
   Info,
   Title,
-  OwnerAvatar,
   Author,
 } from './styles';
 
@@ -22,57 +24,103 @@ export default class User extends Component {
     title: navigation.getParam('user').name,
   });
 
-  constructor() {
-    super();
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
 
-    this.PropTypes = {
-      navigation: propTypes.shape({
-        navigate: propTypes.func,
-      }).isRequired,
-    };
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
 
-    this.state = {
-      stars: [],
-    };
-  }
+  state = {
+    stars: [],
+
+    page: 1,
+
+    loading: true,
+
+    refreshing: false,
+  };
 
   async componentDidMount() {
+    this.load();
+  }
+
+  load = async (page = 1) => {
+    const { stars } = this.state;
+
     const { navigation } = this.props;
+
     const user = navigation.getParam('user');
 
-    const response = await api.get(`/users/${user.login}/starred`);
+    const response = await api.get(`/users/${user.login}/starred`, {
+      params: { page },
+    });
 
     this.setState({
-      stars: response.data,
+      stars: page >= 2 ? [...stars, ...response.data] : response.data,
+
+      page,
+
+      loading: false,
+
+      refreshing: false,
     });
-  }
+  };
+
+  loadMore = () => {
+    const { page } = this.state;
+
+    const nextPage = page + 1;
+
+    this.load(nextPage);
+  };
+
+  refreshList = () => {
+    this.setState({ refreshing: true, stars: [] }, this.load);
+  };
+
+  handleNavigate = repository => {
+    const { navigation } = this.props;
+    navigation.navigate('Repository', { repository });
+  };
 
   render() {
     const { navigation } = this.props;
-    const { stars } = this.state;
-
+    const { stars, loading, refreshing } = this.state;
     const user = navigation.getParam('user');
 
     return (
       <Container>
         <Header>
           <Avatar source={{ uri: user.avatar }} />
+
           <Name>{user.name}</Name>
+
           <Bio>{user.bio}</Bio>
         </Header>
-        <Stars
-          data={stars}
-          keyExtractor={star => String(star.id)}
-          renderItem={({ item }) => (
-            <Starred>
-              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
-              <Info>
-                <Title>{item.name}</Title>
-                <Author>{item.owner.login}</Author>
-              </Info>
-            </Starred>
-          )}
-        />
+
+        {loading ? (
+          <Loading />
+        ) : (
+          <Stars
+            data={stars}
+            onRefresh={this.refreshList}
+            refreshing={refreshing}
+            onEndReachedThreshold={0.2}
+            onEndReached={this.loadMore}
+            keyExtractor={star => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred onPress={() => this.handleNavigate(item)}>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
       </Container>
     );
   }
--- modulo06/src/routes.js
+++ modulo06/src/routes.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 /* Pages */
 import Main from './pages/Main';
+import Repository from './pages/Repository';
 import User from './pages/User';
 
 const Routes = createAppContainer(
   createStackNavigator(
     {
       Main,
+      Repository,
       User,
     },
     {
